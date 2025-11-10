be1c0570ea027dcc34cd47758f387aa2787fc459
probablynotgrady <ipodidiot1000@gmail.com>
2025-11-10 14:28:37 +0000

    amade a jawoefj

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 import "./style.css";
 
-import { GameInterface } from 'simple-canvas-library';
+import { GameInterface, } from "simple-canvas-library";
 
 let gi = new GameInterface();
 
-gi.addDrawing(
-  function ({ ctx, width, height, elapsed }) {
-    // Your drawing code here...
-    // (see demo.js for fuller example)
-  }
-)
+let squareX = 0;
+let squareY = 0;
+gi.addDrawing(function ({ ctx, width, height, elapsed, stepTime }) {
+  ctx.fillRect(squareX, squareY, 20, 20);
+  ctx.fillStyle = "white";
+  
+
+});
+//have square move in different directions and at different speeds when clicked
+gi.addClickHandler(function ({ x, y }) {
+  squareX = x - 10;
+  squareY = y - 10;
+});
 
 gi.run();
-
-
